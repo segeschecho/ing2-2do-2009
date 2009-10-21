16ad74988213671f339e98d4aa8ee2b0e012c252
martinpage2001 <martinpage2001@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-21 23:37:30 +0000

    Manda un mensaje en json bonito... con datos de auditoria y todo...
    una linda pagina para ver mejor lo enviado en json es 
    
    http://jsonformatter.curiousconcept.com/
    
    le pones un texto escrito en json y te lo valida y tabula lindo ...
    escribir en json es casi casi casi lo mismo que un dictionary en phyton!

--- trunk/Prueba Concepto/TR.py
+++ trunk/Prueba Concepto/TR.py
@@ -2,12 +2,16 @@ import sys, time, json, random
 
 def guardar_mensaje(dato, id_men):
 	print "Estoy ecribiendo con id de mensaje : " + str(id_men)
-	archivo = open("TR\\ID TR " + str(id_TR) + " - ID SMS "+ str(id_men)+".tr", "w")
-	archivo.write("Esto es un sms de una TR con id " + str(id_TR) + "\n")
-	dato_json = json.dumps(dato)
+	archivo = open("TR\\Id TR - " + str(id_TR) + " - Id Mensaje - "+ str(id_men) + ".tr", "w")
+	informacion = {'Id TR' : id_TR}
+	informacion['Timestamp'] = time.time()
+	informacion['Id Mensaje'] = id_men
+	informacion['Id Parte'] = 1
+	informacion['Tipo Mensaje'] = 'DATOS'
+	informacion['Contenido'] = dato
+	dato_json = json.dumps(informacion)
 	archivo.write(dato_json)
 	archivo.close()	
-	
 
 def prueba():
 	print 'hoal esto paso despues'
@@ -16,11 +20,11 @@ id_TR = sys.argv[1]
 i = 0
 id_mensaje = 0
 
-while i < 10:
+while i < 4:
 	time.sleep(1)
-	hora = time.time()
 	temperatura = random.random()
-	datos = {'TimeStamp': hora, 'Temperatura': temperatura}
+	humedad = random.randint(0,100)
+	datos = {'Temperatura' : temperatura, 'Humedad' : humedad}
 	guardar_mensaje(datos, id_mensaje)
 	i += 1
 	id_mensaje += 1
