84f10b02750c0609a0726619e6db8fbc046a51e6
martinpage2001 <martinpage2001@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-21 23:06:02 +0000

    Mejore la simulacion .... borra los archivos viejos y ya genera algunos con timestamp y temperatura... codeado en json!

--- trunk/Prueba Concepto/Simulacion.bat
+++ trunk/Prueba Concepto/Simulacion.bat
@@ -1,2 +1,12 @@
+:: Simulacion.bat
+:: Crea los procesos necesarios para
+:: la prueba de concepto
+@ECHO OFF
+del TR\*.tr
+del TR\*.ec
+del EC\*.tr
+del EC\*.ec
 TR.py 1
+ECHO.
 pause
+
--- trunk/Prueba Concepto/TR.py
+++ trunk/Prueba Concepto/TR.py
@@ -1,21 +1,30 @@
-import sys, time
+import sys, time, json, random
 
-def guardar_mensaje(dato):
-	print "Estoy ecribiendo " + str(dato)
-	archivo = open("TR\\ID " + str(id_TR) + " - "+str(dato)+".txt", "w")
-	archivo.write("Esto es un sms de una TR con id " + str(id_TR))
-	archivo.write(str(dato))
+def guardar_mensaje(dato, id_men):
+	print "Estoy ecribiendo con id de mensaje : " + str(id_men)
+	archivo = open("TR\\ID TR " + str(id_TR) + " - ID SMS "+ str(id_men)+".tr", "w")
+	archivo.write("Esto es un sms de una TR con id " + str(id_TR) + "\n")
+	dato_json = json.dumps(dato)
+	archivo.write(dato_json)
 	archivo.close()	
+	
 
 def prueba():
 	print 'hoal esto paso despues'
 
 id_TR = sys.argv[1]
 i = 0
+id_mensaje = 0
+
 while i < 10:
 	time.sleep(1)
-	guardar_mensaje(i)
-	i = i + 1
+	hora = time.time()
+	temperatura = random.random()
+	datos = {'TimeStamp': hora, 'Temperatura': temperatura}
+	guardar_mensaje(datos, id_mensaje)
+	i += 1
+	id_mensaje += 1
+
 	
 #def inicializar_alarma():
 	
