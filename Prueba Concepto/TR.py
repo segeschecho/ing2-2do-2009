20159a0ebbeefb4c03b17774ea79119fb7b3a947
martinpage2001 <martinpage2001@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-21 23:46:07 +0000

    Nada importante... ultimo commit del día... hasta mañana! Suerte que la vida los acompañe y si les da la espalda... tocenle el orto!

--- Prueba Concepto/TR.py
+++ Prueba Concepto/TR.py
@@ -3,18 +3,19 @@ import sys, time, json, random
 def guardar_mensaje(dato, id_men):
 	print "Estoy ecribiendo con id de mensaje : " + str(id_men)
 	archivo = open("TR\\Id TR - " + str(id_TR) + " - Id Mensaje - "+ str(id_men) + ".tr", "w")
-	informacion = {'Id TR' : id_TR}
-	informacion['Timestamp'] = time.time()
-	informacion['Id Mensaje'] = id_men
-	informacion['Id Parte'] = 1
-	informacion['Tipo Mensaje'] = 'DATOS'
+	informacion = datos_auditoria(id_men, 'DATOS')
 	informacion['Contenido'] = dato
 	dato_json = json.dumps(informacion)
 	archivo.write(dato_json)
 	archivo.close()	
 
-def prueba():
-	print 'hoal esto paso despues'
+def datos_auditoria(id_men, tipo_mensaje):
+	informacion = {'Id TR' : id_TR}
+	informacion['Timestamp'] = time.time()
+	informacion['Id Mensaje'] = id_men
+	informacion['Id Parte'] = 1
+	informacion['Tipo Mensaje'] = tipo_mensaje
+	return informacion
 
 id_TR = sys.argv[1]
 i = 0
@@ -23,7 +24,7 @@ id_mensaje = 0
 while i < 4:
 	time.sleep(1)
 	temperatura = random.random()
-	humedad = random.randint(0,100)
+	humedad = random.randint(0, 100)
 	datos = {'Temperatura' : temperatura, 'Humedad' : humedad}
 	guardar_mensaje(datos, id_mensaje)
 	i += 1
