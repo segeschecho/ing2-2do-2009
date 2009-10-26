1d0dc5971b4e0aa8132b318f4ea9aecafd8f24b8
seges.ar <seges.ar@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-26 00:52:12 +0000

    hice lo del timer que queria page, ahora no se que mas hay que hacer con esto jaja

--- Prueba Concepto/Timer.py
+++ Prueba Concepto/Timer.py
@@ -1,16 +1,24 @@
 import time
 import threading
 
-class Timer(threading.Thread):
-	def __init__(self, seconds):
-		self.runTime = seconds
-		threading.Thread.__init__(self)
-	def correr(self, callback, arg):
-		time.sleep(self.runTime)
-		callback(arg)
-		print "Buzzzz!! Time's up!"
 
-#>>> t = Timer(10)
-#>>> t.start()
-#>>> Buzzzz!! Time's up!
+def holass():
+	print 'que choto'
 
+def mensajes(veces, tiempo):
+	print 'ejecutando holass %s veces cada %s segundos'%(veces,tiempo)
+	for i in range(veces):
+		holass()
+		time.sleep(tiempo)
+
+veces = 2
+tiempo = 1
+hiloLoco = threading.Thread(target = mensajes, args = (veces, tiempo))
+
+hiloLoco.start()
+
+hiloLoco.join()
+
+#t = threading.Timer(1.0, holass)
+#t.start()
+#print 'mande el timer'
