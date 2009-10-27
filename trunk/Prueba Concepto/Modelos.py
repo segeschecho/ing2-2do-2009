39349acb5bcd0982e0a95dfd481ec9fa9790d0aa
gonzalocastillo086 <gonzalocastillo086@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-27 23:40:25 +0000

    Subimos la parte de modelos andando y leyendo JSON. En datos1.py hay datos q generan una prediccion de huracan.

--- trunk/Prueba Concepto/Modelos.py
+++ trunk/Prueba Concepto/Modelos.py
@@ -1,17 +1,38 @@
 import sys 
+import random
+import json
+# T  : Temperatura
+# P : Presion
+# H : Humedad
+# V : Velocidad Vientos
+
 
 reglas = sys.argv[1]
-T =  int(sys.argv[2]) 
-V =  int(sys.argv[3]) 
-P =  int(sys.argv[4]) 
-H =  int(sys.argv[5]) 
 
+if len(sys.argv) < 3:
+    #Asignacion De Datos
+    datos = {} #Armara en un protocolo comun lo datos para llas reglas
+    for i in range(1,10):
+        datos[i] = {}
+        datos[i]['T'] = []
+        datos[i]['P'] = []
+        datos[i]['V'] = []
+        datos[i]['H'] = []
 
-#if values.len() < 4:
-#    print 'No se encuentran los valores apropiados'
-
+    for i in range(1,10):
+        for j in range (1,5):
+            datos[i]['T'].append(random.randint(-2,50))
+            datos[i]['P'].append(random.randint(950,1050))
+            datos[i]['V'].append(random.randint(30,300))
+            datos[i]['H'].append(random.randint(0,100))
+else:
+    data = sys.argv[2]
+    archData = open(data)
+    datos = eval(archData.read()) 
+       
+print datos
 archivo = open(reglas)
-arrReglas = []
+#arrReglas = []
 
 #for linea in archivo.readlines():
 #    exec linea
--- trunk/Prueba Concepto/reglas2.py
+++ trunk/Prueba Concepto/reglas2.py
@@ -2,31 +2,34 @@ def prom_variable(variable):
     # Calculo promedio de una variable
     acumProm = 0
     cantTr = 0
-    for medicion in datos
+    for medicion in datos.values():
         mediciones = medicion[variable] 
-        acumTempTr = 0
-        for dato in mediciones
+        acumTr = 0
+        for dato in mediciones:
             acumTr += dato
         promTr = acumTr / len(mediciones)
         acumProm+=promTr
-        cantTr++
+        cantTr += 1
     promTot = acumProm / cantTr
-
+    return promTot
     
-promTemp = prom_variable("Temperatura")
-print "La temperatura promedio es ", promTemp, "/n"
-promHumedad = prom_variable("Humedad")
-print "La humedad promedio es ", promHumedad, "/n"
-promPresion = prom_variable("Presion")
-print "La presion promedio es ", promPresion, "/n"
-promViento = prom_variable("Viento")
-print "La velocidad promedio del viento es ", promViento, "/n"
+promTemp = prom_variable('T')
+print "La temperatura promedio es ", promTemp
+promHumedad = prom_variable('H')
+print "La humedad promedio es ", promHumedad
+promPresion = prom_variable('P')
+print "La presion promedio es ", promPresion
+promViento = prom_variable('V')
+print "La velocidad promedio del viento es ", promViento
 
 #Reglas
-if (promTemp > 33 and promHumedad < 60): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
-if (promTemp < -2 and promHumedad > 98): print 'Se preveen fuertes nevadas con posibles avalanchas'
-if (promTemp > 33 and promHumedad > 98 and promPresion > 1000): print 'Se preveen lluvias fuertes y posbiles innundaciones'
-if (promTemp > 33 and promHumedad > 98 and promPresion > 1000 and promViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
-if (promPresion > 1020 and promViento > 40) print 'Altas probabilidades de tornados y huracanes'
-
+if (promTemp > 25 and promHumedad < 40): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
+if (promTemp < -2 and promHumedad > 90): print 'Se preveen fuertes nevadas con posibles avalanchas'
+if (promTemp > 25 and promHumedad > 98 and promPresion < 1000): print 'Se preveen lluvias fuertes y posibiles innundaciones'
+if (promTemp > 28 and promHumedad > 98 and promPresion < 1000 and promViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
+if (promPresion > 1020 and promViento > 40): print 'Altas probabilidades de tornados y huracanes'
+if (promViento > 100 and promPresion > 960): print 'Se preveen vientos muy fuertes'
+if (promTemp > 10 and promTemp <= 25 and promViento < 40 and promHumedad < 90): print 'Tiempo despejado, condiciones normales'
+if (promTemp > 6 and promTemp < 24 and promHumedad > 90): print 'Se pronostica niebla, y visibilidad cuasi nula en rutas y caminos'
+if (promTemp >= 6 and promTemp <= 24 and promHumedad <= 60): print 'No se preveen grandes variaciones de presion y temperatura en los proximos minutos.'
 
