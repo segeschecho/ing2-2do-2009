a34744285d0068128e17b099baa42a3b218551a9
gonzalocastillo086 <gonzalocastillo086@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-27 23:40:25 +0000

    Subimos la parte de modelos andando y leyendo JSON. En datos1.py hay datos q generan una prediccion de huracan.

--- Prueba Concepto/reglas1.py
+++ Prueba Concepto/reglas1.py
@@ -1,27 +1,33 @@
 def max_variable(variable):
     # Calculo max de una variable
     acummax = -50
-    for medicion in datos
+    for medicion in datos.values():
         mediciones = medicion[variable] 
         medMax = max(mediciones)
-        if (medMax > acummax) acummax = medMax
-    
+        if (medMax > acummax): 
+            acummax = medMax
+    return acummax
 
     
-maxTemp = max_variable("Temperatura")
-print "La temperatura maxedio es ", maxTemp, "/n"
-maxHumedad = max_variable("Humedad")
-print "La humedad maxedio es ", maxHumedad, "/n"
-maxPresion = max_variable("Presion")
-print "La presion maxedio es ", maxPresion, "/n"
-maxViento = max_variable("Viento")
-print "La velocidad maxedio del viento es ", maxViento, "/n"
+maxTemp = max_variable('T')
+print "La temperatura maxima es ", maxTemp
+maxHumedad = max_variable('H')
+print "La humedad maxima es ", maxHumedad
+maxPresion = max_variable('P')
+print "La presion maxima es ", maxPresion
+maxViento = max_variable('V')
+print "La velocidad maxima del viento es ", maxViento
 
 #Reglas
-if (maxTemp > 33 and maxHumedad < 60): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
-if (maxTemp < -2 and maxHumedad > 98): print 'Se preveen fuertes nevadas con posibles avalanchas'
-if (maxTemp > 33 and maxHumedad > 98 and maxPresion > 1000): print 'Se preveen lluvias fuertes y posbiles innundaciones'
-if (maxTemp > 33 and maxHumedad > 98 and maxPresion > 1000 and maxViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
-if (maxPresion > 1020 and maxViento > 35) print 'Altas probabilidades de tornados y huracanes'
+#Reglas
+if (maxTemp > 25 and maxHumedad < 40): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
+if (maxTemp < -2 and maxHumedad > 90): print 'Se preveen fuertes nevadas con posibles avalanchas'
+if (maxTemp > 25 and maxHumedad > 98 and maxPresion < 1000): print 'Se preveen lluvias fuertes y posibiles innundaciones'
+if (maxTemp > 28 and maxHumedad > 98 and maxPresion < 1000 and maxViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
+if (maxPresion > 1020 and maxViento > 40): print 'Altas probabilidades de tornados y huracanes'
+if (maxViento > 100 and maxPresion > 960): print 'Se preveen vientos muy fuertes'
+if (maxTemp > 10 and maxTemp <= 25 and maxViento < 40 and maxHumedad < 90): print 'Tiempo despejado, condiciones normales'
+if (maxTemp > 6 and maxTemp < 24 and maxHumedad > 90): print 'Se pronostica niebla, y visibilidad cuasi nula en rutas y caminos'
+if (maxTemp >= 6 and maxTemp <= 24 and maxHumedad <= 60): print 'No se preveen grandes variaciones de presion y temperatura en los proximos minutos.'
 
 
