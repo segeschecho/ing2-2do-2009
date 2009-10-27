74be9fffec73798fe036bcf792c74cdd5eec14d1
kivielizalde <kivielizalde@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-27 21:44:22 +0000

    Subo las nuevas reglas

--- Prueba Concepto/reglas1.py
+++ Prueba Concepto/reglas1.py
@@ -1,7 +1,27 @@
-if (T > 33 and H > 60): print 'Habra precipitaciones en los proximos 10 minutos'
-if (T < 33 or H < 60): print'No habra precipitaciones a partir de los proximos 10 minutos'
-if (P > 60 and H > 50 and V > 600): print 'Existencia de posibles tornados en 10 minutos!!!'
-if (H > 60 and P > 50): print 'La temperatura a partir de los proximos 10 minutos sera de', int(T) + 1, 'grados.' 
-if (H < 60 and P < 50): print'La temperatura a partir de los proximos 10 minutos sera de', int(T) - 1, 'grados.'
-if (H > 60 and P < 50): print 'La temperatura a partir de los proximos 10 minutos sera de', int(T), 'grados.'
-if (H < 60 and P > 50): print 'La temperatura a partir de los proximos 10 minutos sera de', int(T), 'grados.'
+def max_variable(variable):
+    # Calculo max de una variable
+    acummax = -50
+    for medicion in datos
+        mediciones = medicion[variable] 
+        medMax = max(mediciones)
+        if (medMax > acummax) acummax = medMax
+    
+
+    
+maxTemp = max_variable("Temperatura")
+print "La temperatura maxedio es ", maxTemp, "/n"
+maxHumedad = max_variable("Humedad")
+print "La humedad maxedio es ", maxHumedad, "/n"
+maxPresion = max_variable("Presion")
+print "La presion maxedio es ", maxPresion, "/n"
+maxViento = max_variable("Viento")
+print "La velocidad maxedio del viento es ", maxViento, "/n"
+
+#Reglas
+if (maxTemp > 33 and maxHumedad < 60): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
+if (maxTemp < -2 and maxHumedad > 98): print 'Se preveen fuertes nevadas con posibles avalanchas'
+if (maxTemp > 33 and maxHumedad > 98 and maxPresion > 1000): print 'Se preveen lluvias fuertes y posbiles innundaciones'
+if (maxTemp > 33 and maxHumedad > 98 and maxPresion > 1000 and maxViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
+if (maxPresion > 1020 and maxViento > 35) print 'Altas probabilidades de tornados y huracanes'
+
+
