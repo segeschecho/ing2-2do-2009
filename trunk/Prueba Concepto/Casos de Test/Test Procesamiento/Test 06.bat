cb1b23e8111e05373b36d8db13258c51a93ee801
gonzalocastillo086 <gonzalocastillo086@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-29 15:32:47 +0000

    Subo la parte de modulos dearquitectura, la parte de modelos para la prueba de concepto y los casos de test asociados a ellos

---
+++ trunk/Prueba Concepto/Casos de Test/Test Procesamiento/Test 06.bat
@@ -0,0 +1,12 @@
+:: Test 06.bat
+:: Sexto caso de test
+@echo off
+echo **************************************************** 
+echo Caso de Test 06 (Modulo Procsamiento)
+echo Descripcion :
+echo    Se utilizan las reglas2.py y no se le pasa
+echo    ningun archivo de datos. 
+echo **************************************************** 
+python ..\..\Modelos.py ..\..\reglas2.py 
+pause
+echo on
--- trunk/Prueba Concepto/reglas2.py
+++ trunk/Prueba Concepto/reglas2.py
@@ -20,16 +20,16 @@ print "La humedad promedio es ", promHumedad
 promPresion = prom_variable('P')
 print "La presion promedio es ", promPresion
 promViento = prom_variable('V')
-print "La velocidad promedio del viento es ", promViento
+print "La velocidad promedio del viento es ", promViento, "\n"
 
 #Reglas
-if (promTemp > 25 and promHumedad < 40): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales'
-if (promTemp < -2 and promHumedad > 90): print 'Se preveen fuertes nevadas con posibles avalanchas'
-if (promTemp > 25 and promHumedad > 98 and promPresion < 1000): print 'Se preveen lluvias fuertes y posibiles innundaciones'
-if (promTemp > 28 and promHumedad > 98 and promPresion < 1000 and promViento > 20): print 'Se preveen tsunamis en las ciudades costeras'
-if (promPresion > 1020 and promViento > 40): print 'Altas probabilidades de tornados y huracanes'
-if (promViento > 100 and promPresion > 960): print 'Se preveen vientos muy fuertes'
-if (promTemp > 10 and promTemp <= 25 and promViento < 40 and promHumedad < 90): print 'Tiempo despejado, condiciones normales'
-if (promTemp > 6 and promTemp < 24 and promHumedad > 90): print 'Se pronostica niebla, y visibilidad cuasi nula en rutas y caminos'
-if (promTemp >= 6 and promTemp <= 24 and promHumedad <= 60): print 'No se preveen grandes variaciones de presion y temperatura en los proximos minutos.'
+if (promTemp > 25 and promHumedad < 40): print 'Debido a la alta temperatura y baja humedad hay probabilidades de incendios forestales... \n'
+if (promTemp < -2 and promHumedad > 90): print 'Se preveen fuertes nevadas con posibles avalanchas... \n'
+if (promTemp > 25 and promHumedad > 98 and promPresion < 1000): print 'Se preveen lluvias fuertes y posibles innundaciones...\n'
+if (promTemp > 28 and promHumedad > 98 and promPresion < 1000 and promViento > 20): print 'Se preveen tsunamis en las ciudades costeras...\n'
+if (promPresion > 1020 and promViento > 40): print 'Altas probabilidades de tornados y huracanes...\n'
+if (promViento > 100 and promPresion > 960): print 'Se preveen vientos muy fuertes...\n'
+if (promTemp > 10 and promTemp <= 25 and promViento < 40 and promHumedad < 90): print 'Tiempo despejado, condiciones normales...\n'
+if (promTemp > 6 and promTemp < 24 and promHumedad > 90): print 'Se pronostica niebla, y visibilidad cuasi nula en rutas y caminos...\n'
+if (promTemp >= 6 and promTemp <= 24 and promHumedad <= 60): print 'No se preveen grandes variaciones de presion y temperatura en los proximos minutos...\n'
 
