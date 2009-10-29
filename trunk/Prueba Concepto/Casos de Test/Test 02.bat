71d9fecb4626f0734cbd269a0fc58366dde2c133
martinpage2001 <martinpage2001@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-29 00:14:45 +0000

    Agrego dos Casos de Test
    En total hay tres
    1. Se cae una tr y luego de un rato se levanta
    2. No se cae nada... funcionamiento normal
    3. La EC cree que se cae la EC por que esta manda más lento de lo que asume la ec como tiempo de caida
    
    en este ultimo tambien agregue que las TR se levanten con aglun tiempo entre ellas 3 seg!

--- trunk/Prueba Concepto/Casos de Test/Test 02.bat
+++ trunk/Prueba Concepto/Casos de Test/Test 02.bat
@@ -1,15 +1,13 @@
-:: Test 01.bat
-:: Primer caso de test
+:: Test 02.bat
+:: Segundo caso de test
 @echo off
 del TR\*.tr
 del EC\*.ec
 echo **************************************************** 
-echo Caso de Test 01
+echo Caso de Test 02
 echo Descripcion :
 echo    Existen 5 TR que envian cada 20 segundos y
-echo    una se cae a los 45 segundos.
-echo    Luego de esto 50 seg más tarde la TR se recupera
-echo    y la EC lo reconoce!
+echo    ninguna se cae.
 echo **************************************************** 
 ::start Canal.py
 ::start RecepcionSegura.py
@@ -20,15 +18,10 @@ echo Levanto La RecepcionSegura de la EC
 start ..\RecepcionSegura.py 40
 :: Las TR's tienen 4 parametros : intervalo, cantidad , Id TR, tiempo vida
 echo Levanto las 5 TR
-start ..\TR.py 20 4 1 45
-start ..\TR.py 20 9 2 80000
-start ..\TR.py 20 9 3 80000
-start ..\TR.py 20 9 4 80000
-start ..\TR.py 20 9 5 80000
-Pause.py 45
-echo Se cae TR 1
-Pause.py 50
-echo Se levanta TR 1
-start ..\TR.py 20 3 1 80000
+start ..\TR.py 20 15 1 80000
+start ..\TR.py 20 15 2 80000
+start ..\TR.py 20 15 3 80000
+start ..\TR.py 20 15 4 80000
+start ..\TR.py 20 15 5 80000
 pause
 echo on
