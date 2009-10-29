baed5e8a29dc707c6f61bd00eba20c3f8dfc807c
gonzalocastillo086 <gonzalocastillo086@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-10-29 15:32:47 +0000

    Subo la parte de modulos dearquitectura, la parte de modelos para la prueba de concepto y los casos de test asociados a ellos

--- Prueba Concepto/Modelos.py
+++ Prueba Concepto/Modelos.py
@@ -6,12 +6,17 @@ import json
 # H : Humedad
 # V : Velocidad Vientos
 
+if len(sys.argv) < 2:
+    print 'Falta archivo de reglas imposible procesar... \n'
+    print 'Ejecutar python Modelos.py reglasX.py datosX.py(opcional)'
+    sys.exit(1)
+
 
 reglas = sys.argv[1]
 
 if len(sys.argv) < 3:
     #Asignacion De Datos
-    datos = {} #Armara en un protocolo comun lo datos para llas reglas
+    datos = {} #Arma en una esructura comun lo datos para las reglas
     for i in range(1,10):
         datos[i] = {}
         datos[i]['T'] = []
@@ -30,12 +35,9 @@ else:
     archData = open(data)
     datos = eval(archData.read()) 
        
-print datos
+#Descomentando la siguiente sentencia se puede ver el diccionario de datos armado.
+#print datos
 archivo = open(reglas)
-#arrReglas = []
-
-#for linea in archivo.readlines():
-#    exec linea
 
 exec archivo.read()
     
