ab8c06dd70de32977c65e55398713c550632742f
gonzalocastillo086 <gonzalocastillo086@d44e82e0-a49a-11de-8ba5-91705c1537ed>
2009-12-08 18:37:30 +0000

    subo mas Casos de Test

--- trunk/Prueba Concepto/ColModelos/Test/Test 03.bat
+++ trunk/Prueba Concepto/ColModelos/Test/Test 03.bat
@@ -1,22 +1,20 @@
-:: Test 01.bat
-:: Primer caso de test
+:: Test 03.bat
+:: Tercer caso de test
 @echo off
 del BD\*.pu
 del Resultados\*.ec
 echo **************************************************** 
-echo Test 01: Cuatro Estaciones Centrales se suscriben 
-echo entre si, para intercambiar datos de sus modelos
+echo Test 03: Tres Estaciones Centrales no se suscriben 
+echo a ninguna por lo cual no se debe pasar ningun resultado.
 echo **************************************************** 
 echo Levanto las ECs.
 :: Las ECs tienen dos parametros : id ec, dicc de otra id ec en a array de tipos de resultdos
 start ..\EC.py  1 "{}"
 echo Levantada EC 1...OK.
-start ..\EC.py  2 "{\"1\":[\"TOTAL\"]}" 
+start ..\EC.py  2 "{}" 
 echo Levantada EC 2...OK.
-start ..\EC.py  3 "{\"1\":[\"TOTAL\"],\"2\":[\"PARCIAL\"]}"
+start ..\EC.py  3 "{}"
 echo Levantada EC 3...OK.
-start ..\EC.py  4 "{\"3\":[\"PARCIAL\"],\"1\":[\"PARCIAL\", \"TOTAL\"]}"
-echo Levantada EC 4...OK.
 echo Se levantaron correctamente todas las ECs.
 pause
 echo on
